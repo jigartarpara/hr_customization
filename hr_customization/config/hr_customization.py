8cea1546d114192156a3a5ca232073496161a4f0
Jigar Tarpara <jigartarpara68@gmail.com>
2020-12-15 14:44:19 +0530

    HR Customization

---
+++ hr_customization/config/hr_customization.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Recruitment"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Job Description",
+					"description": _("Job Description")
+				},
+				{
+					"type": "doctype",
+					"name": "Job Vacancy",
+					"description": _("Job Vacancy")
+				},
+				{
+					"type": "doctype",
+					"name": "Job Application",
+					"description": _("Job Application"),
+				},
+				{
+					"type": "doctype",
+					"name": "Job Offer Letter",
+					"description": _("Job Offer Letter"),
+				},
+				{
+					"type": "doctype",
+					"name": "Employee Onboarding",
+					"description": _("Employee Onboarding"),
+				}
+			]
+		},
+	]
