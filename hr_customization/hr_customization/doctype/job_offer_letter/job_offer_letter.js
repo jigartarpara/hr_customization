b38e8035eefda7a24d3cdbf60c7531bf6cd3a350
Jigar Tarpara <jigartarpara68@gmail.com>
2020-12-19 15:09:22 +0530

    Chages

--- hr_customization/hr_customization/doctype/job_offer_letter/job_offer_letter.js
+++ hr_customization/hr_customization/doctype/job_offer_letter/job_offer_letter.js
@@ -3,14 +3,14 @@
 
 frappe.ui.form.on('Job Offer Letter', {
 	refresh: function(frm) {
-		// if(!cur_frm.doc.__islocal) {;
-		// 	var job_vacancy = frm.add_custom_button(__('Employee Onboarding Process'), function(){
-		// 		frappe.model.open_mapped_doc({
-		// 			method: "hr_customization.hr_customization.doctype.job_offer_letter.job_offer_letter.make_job_offer_letter",
-		// 			frm: cur_frm
-		// 		})
-		// 	})
-		// 	job_vacancy.addClass('btn-primary')
-		// }
+		if(!cur_frm.doc.__islocal) {;
+			var job_vacancy = frm.add_custom_button(__('Employee Onboarding Process'), function(){
+				frappe.model.open_mapped_doc({
+					method: "hr_customization.hr_customization.doctype.job_offer_letter.job_offer_letter.make_employee_onboarding",
+					frm: cur_frm
+				})
+			})
+			job_vacancy.addClass('btn-primary')
+		}
 	},
 });
--- hr_customization/hr_customization/doctype/job_offer_letter/job_offer_letter.py
+++ hr_customization/hr_customization/doctype/job_offer_letter/job_offer_letter.py
@@ -5,8 +5,28 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
+from frappe.model.mapper import get_mapped_doc
 class JobOfferLetter(Document):
 	def validate(self):
 		if not self.skip_restriction and not self.job_application:
 			frappe.throw("Job Vacancy is Mandatory")
+
+@frappe.whitelist()
+def make_employee_onboarding(source_name, target_doc=None, ignore_permissions = False):	
+	doclist = get_mapped_doc("Job Offer Letter", source_name, {
+			"Job Offer Letter": {
+				"doctype": "Employee Onboarding Process",
+				"field_map": {
+					"name" : "job_offer_letter"
+				}
+			},
+			# "Production SO Table": {
+			# 	"doctype": "Paper Cup Job Order Item",
+			# 	"field_map": {
+			# 		"item" : "item",
+			# 		"manufacture_carton": "carton",
+			# 	}
+			# }
+		}, target_doc, ignore_permissions=ignore_permissions)
+	
+	return doclist
