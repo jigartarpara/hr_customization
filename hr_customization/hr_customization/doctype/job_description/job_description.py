6bf74a73e4bc6fb071d8d9435bc2f01dfb09b730
Jigar Tarpara <jigartarpara68@gmail.com>
2020-12-19 14:20:01 +0530

    HR Module - Button For Next Flow

--- hr_customization/hr_customization/doctype/job_description/job_description.py
+++ hr_customization/hr_customization/doctype/job_description/job_description.py
@@ -3,8 +3,28 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.model.mapper import get_mapped_doc
 class JobDescription(Document):
 	pass
+
+@frappe.whitelist()
+def make_jobvacancy(source_name, target_doc=None, ignore_permissions = False):	
+	doclist = get_mapped_doc("Job Description", source_name, {
+			"Job Description": {
+				"doctype": "Job Vacancy",
+				"field_map": {
+					"name" : "title"
+				}
+			},
+			# "Production SO Table": {
+			# 	"doctype": "Paper Cup Job Order Item",
+			# 	"field_map": {
+			# 		"item" : "item",
+			# 		"manufacture_carton": "carton",
+			# 	}
+			# }
+		}, target_doc, ignore_permissions=ignore_permissions)
+	
+	return doclist
--- hr_customization/hr_customization/doctype/job_vacancy/job_vacancy.js
+++ hr_customization/hr_customization/doctype/job_vacancy/job_vacancy.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Job Vacancy', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if(!cur_frm.doc.__islocal) {;
+			var job_vacancy = frm.add_custom_button(__('Job Application'), function(){
+				frappe.model.open_mapped_doc({
+					method: "hr_customization.hr_customization.doctype.job_vacancy.job_vacancy.make_job_application",
+					frm: cur_frm
+				})
+			})
+			job_vacancy.addClass('btn-primary')
+		}
+	}
 });
