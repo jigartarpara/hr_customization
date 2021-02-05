7ece4e36534d7c7b9cf19e6ecb137ed4760bffa7
Jigar Tarpara <jigartarpara68@gmail.com>
2021-02-05 15:38:42 +0530

    Chages

--- hr_customization/hr_customization/doctype/employee_onboarding_process/employee_onboarding_process.py
+++ hr_customization/hr_customization/doctype/employee_onboarding_process/employee_onboarding_process.py
@@ -10,3 +10,17 @@ class EmployeeOnboardingProcess(Document):
 	def validate(self):
 		if not self.skip_restriction and not self.job_offer_letter:
 			frappe.throw("JOB Offer Letter is Mandatory")
+		
+		if self.get('workflow_state') and self.workflow_state == "Pending For Biometric Registration"  and (not self.employee):
+			frappe.throw("Couldn't Pending For Biometric Registration because employee not available")
+		
+		if self.get('workflow_state') and self.workflow_state == "Biometric Registration Done"  and (not self.card_number):
+			frappe.throw("Couldn't Biometric Registration Done because Card Number not available")
+		
+		if self.get('workflow_state') and self.workflow_state == "Resources Pending"  and (not self.induction_checklist):
+			frappe.throw("Couldn't Resources Pending because Resources Table not available")
+		
+		if self.get('workflow_state') and self.workflow_state == "Resources Provided" :
+			for raw in self.induction_checklist:
+				if raw.status != "Done":
+					frappe.throw("Couldn't Resources Provided because Resource Status not done yet.")
